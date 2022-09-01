f5c9c0e9cd9e87b16b3b286bd5c0723bb35e39cd
tokarvasyl1 <tokarvasyl1@gmail.com>
2022-09-01 18:10:45 +0300

    burger menu ready

--- js/script.js
+++ js/script.js
@@ -50,6 +50,7 @@ if(isMobile.any()){
 $(document).ready(function (){
     $('.header_burger').click(function(event) {
         $('.header_burger,.menu').toggleClass('active');
+        $('body').toggleClass('lock');
     })
 })
 
